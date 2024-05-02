3222070f77bcbd3a53f69e5a829c9fb21d6ee4c2
Yavuz Selim Tokdemir <yavuzselim.tokdemir@outlook.com>
2024-05-02 13:29:39 +0300

    Update fan speed using slider in flutter and store the value in in Firebase Realtime Database

--- lib/fanpage.dart
+++ lib/fanpage.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-class FanPage extends StatelessWidget {
+class FanPage extends StatefulWidget {
+  @override
+  _FanPageState createState() => _FanPageState();
+}
+
+class _FanPageState extends State<FanPage> {
+  double _progressValue = 0.0;
+
+  // Firebase veritabanı referansını oluşturun
+  final DatabaseReference _fanSpeedRef =
+      FirebaseDatabase.instance.reference().child('fan_speed');
+
+  void _updateProgress(double value) {
+    setState(() {
+      _progressValue = value;
+    });
+
+    // Değer 100'e bölünerek Firebase veritabanına gönderilir
+    _fanSpeedRef.set((value).toInt());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +32,57 @@ class FanPage extends StatelessWidget {
         title: Text('Fan'),
       ),
       body: Center(
-        child: Text(''),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: CircularProgressIndicator(
+                    backgroundColor: Color(0xFF222831),
+                    // Değeri 100'e böldükten sonra kullan
+                    value: _progressValue / 100,
+                    strokeWidth: 10,
+                    valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
+                  ),
+                ),
+                Positioned(
+                  top: 60.0, // Ayarlama gerektiğinde konumu ayarlayın
+                  child: Text(
+                    "Fan Hızı",
+                    style: TextStyle(
+                      fontSize: 24, // Her iki metni de aynı boyuta ayarlayın
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: 70.0, // Ayarlama gerektiğinde konumu ayarlayın
+                  child: Text(
+                    "${(_progressValue).toInt()}",
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors
+                            .white), // Her iki metni de aynı boyuta ayarlayın
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Slider(
+              value: _progressValue,
+              onChanged: _updateProgress,
+              min: 0,
+              max: 100,
+              divisions: 100,
+              activeColor: Colors.blue,
+              inactiveColor: Color(0xFF222831),
+            ),
+          ],
+        ),
       ),
     );
   }
