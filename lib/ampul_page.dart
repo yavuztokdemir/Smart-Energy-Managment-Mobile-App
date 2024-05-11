b738657bf227155d5f5fc8b54ceaeb600003d13a
Yavuz Selim Tokdemir <yavuzselim.tokdemir@outlook.com>
2024-05-11 19:06:24 +0300

    The brightness adjusment slide was replaced with on/off button

--- lib/ampul_page.dart
+++ lib/ampul_page.dart
@@ -7,22 +7,19 @@ class AmpulPage extends StatefulWidget {
 }
 
 class _AmpulPageState extends State<AmpulPage> {
-  int brightnessPercentage = 0; // Başlangıçta parlaklık yüzdesi 0
+  bool lightState = false; // Ampulün başlangıçta kapalı olduğunu varsayalım
   bool motionDetectionMode = false; // Hareket algılama modu
 
-  Future<void> updateBrightness(double percentage) async {
-    // Yüzde değerini tam sayıya yuvarla
-    int brightnessValue = percentage.round();
-
+  Future<void> updateLightState(bool state) async {
     // Firebase Realtime Database referansı oluştur
     final DatabaseReference databaseRef = FirebaseDatabase.instance.ref();
 
-    // Firebase Realtime Database'e parlaklık değerini güncelle
-    await databaseRef.child('brightness').set(brightnessValue);
+    // Firebase Realtime Database'e ışık durumunu güncelle
+    await databaseRef.child('light').set(state ? 1 : 0);
 
     // setState ile yeniden çizimi tetikle
     setState(() {
-      brightnessPercentage = brightnessValue;
+      lightState = state;
     });
   }
 
@@ -47,46 +44,32 @@ class _AmpulPageState extends State<AmpulPage> {
         title: Text('Ampül'),
         backgroundColor: Color(0xFF222831),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Hareket algılama modunu güncelle
-              updateMotionDetectionMode(!motionDetectionMode);
-            },
-            child: Text(
-              motionDetectionMode ? 'Motion Off' : 'Motion On',
+      body: Center(
+        // Wrap the column with Center widget
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Already set for vertical centering
+          children: [
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                updateLightState(!lightState);
+              },
+              child: Text(
+                lightState ? 'Kapat' : 'Aç',
+              ),
             ),
-          ),
-          SizedBox(height: 20),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Parlaklık: $brightnessPercentage%', // Kullanıcıya seçtiği yüzde değerini göster
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.white,
-                  ),
-                ),
-                Slider(
-                  value: brightnessPercentage.toDouble(),
-                  min: 0,
-                  max: 100,
-                  onChanged: (value) {
-                    // Parlaklık yüzdesi değiştiğinde updateBrightness metodunu çağır
-                    updateBrightness(value);
-                  },
-                  activeColor: Color.fromARGB(255, 118, 171, 174),
-                  inactiveColor: Colors.grey,
-                ),
-              ],
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                updateMotionDetectionMode(!motionDetectionMode);
+              },
+              child: Text(
+                motionDetectionMode ? 'Motion Off' : 'Motion On',
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
